37acf0e6a4358cd2cb1dfdf7e112d843da04f410
Mason Lee <mdudelee@gmail.com>
2024-09-02 12:39:23 -0400

    Started Powershell Basics

---
+++ week2/PowershellBasics.ps1
@@ -0,0 +1,13 @@
+﻿clear
+
+#Q1
+#(Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.InterfaceAlias -ilike "Ethernet0" }).IPAddress
+
+#Q2
+#(Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.InterfaceAlias -ilike "Ethernet0" }).PrefixLength
+
+#Q3
+#Get-WmiObject -List | Where-Object { $_.Name -ilike "Win32_Net*" }
+
+#Q4
+Get-WmiObject -List | Where-Object { $_.Name -ilike "Win32_Net*" } | Sort-Object
