e6221190d02178df4c38ca9b4cc7700878144ea0
Mason Lee <mdudelee@gmail.com>
2024-09-02 11:52:20 -0400

    Uploading process management 1

---
+++ week2/ProcessManagement1.ps1
@@ -0,0 +1,24 @@
+﻿clear
+
+# Q1
+#Get-Process | Where-Object {$_.Name -ilike "C*"}
+
+# Q2
+#$noSys32 = Get-Process | Where-Object {$_.Path -inotlike "*system32*"}
+#$noSys32 | Select ProcessName, Path | Out-String
+
+# Q3
+#$stoppedServices = Get-Service | Where-Object {$_.Status -ilike "Stopped"} | Sort-Object
+#$stoppedServices | Out-String
+#$stoppedServices | Export-Csv -Path $PSScriptRoot"\ServiceData.csv" -NoTypeInformation
+
+#Q4
+$chromeProcess = Get-Process | Where-Object {$_.Name -ilike "chrome"}
+if($chromeProcess)
+{
+    Stop-Process -Name chrome
+}
+else
+{
+    Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome.exe" -ArgumentList '--start "https://www.champlain.edu/"'
+}
