d4c6d224a05434e7de067b63c2dbc53a342a250a
Mason Lee <mdudelee@gmail.com>
2024-09-09 11:49:50 -0400

    Started functions and event logs assignment

---
+++ week3/FunctionsAndEventLogs.ps1
@@ -0,0 +1,55 @@
+﻿.(Join-Path $PSScriptRoot functions.ps1)
+
+clear
+
+#Q1
+#Get-EventLog System -Source Microsoft-Windows-Winlogon
+
+#Q2
+#$loginouts = Get-EventLog System -Source Microsoft-Windows-Winlogon -After (Get-Date).AddDays(-14)
+#
+#$loginoutsTable = @() #Empty array
+#for($i=0; $i -lt $loginouts.Count; $i++){
+#    
+#    $event = ""
+#    if($loginouts[$i].InstanceId -eq 7001) {$event = "Logon"}
+#    if($loginouts[$i].InstanceId -eq 7002) {$event = "Logoff"}
+#
+#    $user = $loginouts[$i].ReplacementStrings[1]
+#
+#    $loginoutsTable += [pscustomobject]@{"Time" = $loginouts[$i].TimeGenerated; `
+#                                        "Id" = $loginouts[$i].EventId; `
+#                                        "Event" = $event; `
+#                                        "User" = $user;
+#                                        }
+#}
+#$loginoutsTable
+
+#Q3
+<#
+$loginouts = Get-EventLog System -Source Microsoft-Windows-Winlogon -After (Get-Date).AddDays(-14)
+
+$loginoutsTable = @() #Empty array
+for($i=0; $i -lt $loginouts.Count; $i++){
+    
+    $event = ""
+    if($loginouts[$i].InstanceId -eq 7001) {$event = "Logon"}
+    if($loginouts[$i].InstanceId -eq 7002) {$event = "Logoff"}
+
+    $user = New-Object System.Security.Principal.SecurityIdentifier($loginouts[$i].ReplacementStrings[1])
+    $user = $user.Translate([System.Security.Principal.NTAccount])
+
+    $loginoutsTable += [pscustomobject]@{"Time" = $loginouts[$i].TimeGenerated; `
+                                        "Id" = $loginouts[$i].EventId; `
+                                        "Event" = $event; `
+                                        "User" = $user;
+                                        }
+}
+$loginoutsTable
+#>
+
+#Q4
+#getLogonoffTable(5);
+
+#Q5
+Get-EventLog System | Where-Object { $_.InstanceId -eq 1074}#Microsoft-Windows-Winlogon -After (Get-Date).AddDays(-5)
---
+++ week3/functions.ps1
@@ -0,0 +1,54 @@
+﻿<#***********************************************
+Function: Get Logon Logoff Table
+Input:    1)int: number of days to retrieve logs from
+Output:   Table of logon logoff logs 
+***********************************************#>
+function getLogonoffTable($numberOfDays){
+    $loginouts = Get-EventLog System -Source Microsoft-Windows-Winlogon -After (Get-Date).AddDays(-$numberOfDays)
+
+    $loginoutsTable = @() #Empty array
+    for($i=0; $i -lt $loginouts.Count; $i++){
+    
+        $event = ""
+        if($loginouts[$i].InstanceId -eq 7001) {$event = "Logon"}
+        if($loginouts[$i].InstanceId -eq 7002) {$event = "Logoff"}
+
+        $user = New-Object System.Security.Principal.SecurityIdentifier($loginouts[$i].ReplacementStrings[1])
+        $user = $user.Translate([System.Security.Principal.NTAccount])
+
+        $loginoutsTable += [pscustomobject]@{"Time" = $loginouts[$i].TimeGenerated; `
+                                            "Id" = $loginouts[$i].EventId; `
+                                            "Event" = $event; `
+                                            "User" = $user;
+                                            }
+    }
+    return $loginoutsTable
+}
+
+
+<#***********************************************
+Function: Get start and shutdown Table
+Input:    1)int: number of days to retrieve logs from
+Output:   Table of start and shutdown logs 
+***********************************************#>
+function getLogonoffTable($numberOfDays){
+    $loginouts = Get-EventLog System -Source Microsoft-Windows-Winlogon -After (Get-Date).AddDays(-$numberOfDays)
+
+    $loginoutsTable = @() #Empty array
+    for($i=0; $i -lt $loginouts.Count; $i++){
+    
+        $event = ""
+        if($loginouts[$i].InstanceId -eq 7001) {$event = "Logon"}
+        if($loginouts[$i].InstanceId -eq 7002) {$event = "Logoff"}
+
+        $user = New-Object System.Security.Principal.SecurityIdentifier($loginouts[$i].ReplacementStrings[1])
+        $user = $user.Translate([System.Security.Principal.NTAccount])
+
+        $loginoutsTable += [pscustomobject]@{"Time" = $loginouts[$i].TimeGenerated; `
+                                            "Id" = $loginouts[$i].EventId; `
+                                            "Event" = $event; `
+                                            "User" = $user;
+                                            }
+    }
+    return $loginoutsTable
+}
