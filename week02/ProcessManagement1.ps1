83d9e8239c67194f4e39c26625bc053287bf7b13
Mason Lee <mdudelee@gmail.com>
2024-09-30 12:18:19 -0400

    Updated startChromeProcess

--- week02/ProcessManagement1.ps1
+++ week02/ProcessManagement1.ps1
@@ -13,12 +13,14 @@
 #$stoppedServices | Export-Csv -Path $PSScriptRoot"\ServiceData.csv" -NoTypeInformation
 
 #Q4
-$chromeProcess = Get-Process | Where-Object {$_.Name -ilike "chrome"}
-if($chromeProcess)
-{
-    Stop-Process -Name chrome
-}
-else
-{
-    Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome.exe" -ArgumentList '--start "https://www.champlain.edu/"'
+function startChromeProcess(){
+    $chromeProcess = Get-Process | Where-Object {$_.Name -ilike "chrome"}
+    if($chromeProcess)
+    {
+        Stop-Process -Name chrome
+    }
+    else
+    {
+        Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome.exe" -ArgumentList '--start "https://www.champlain.edu/"'
+    }
 }
---
+++ week06/turnToMenu.ps1
@@ -0,0 +1,25 @@
+﻿clear
+
+$Prompt = "`n"
+$Prompt += "Please choose your operation:`n"
+$Prompt += "1 - Display last 10 apache logs`n"
+$Prompt += "2 - Display last 10 failed logins for all users`n"
+$Prompt += "3 - Display at risk users`n"
+$Prompt += "4 - Start Chrome web browser and navigate it to champlain.edu`n"
+$Prompt += "5 - Exit`n"
+
+$operation = $true
+
+while($operation){
+
+    
+    Write-Host $Prompt | Out-String
+    $choice = Read-Host 
+
+    if($choice -eq 5){
+        Write-Host "Goodbye" | Out-String
+        exit
+        $operation = $false 
+    }
+
+}
